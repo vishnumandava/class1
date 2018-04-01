2e06a433878ff7408819ea0f68af4783fe89d5a7
NAGAVISHNU MANDAVA <vshnchowdary@gmail.com>
2018-04-01 11:05:13 -0500

    list

---
+++ src/com/javaclass/ListExample.java
@@ -0,0 +1,19 @@
+package com.javaclass;
+
+import java.awt.List;
+import java.util.Arrays;
+
+public class ListExample {
+	    public static void main(String[] args) {
+	        
+	        java.util.List<Integer> list = Arrays.asList(3,2,1,4,5,6,6);
+
+	      
+	        for (Integer integer : list) {
+	            System.out.println(integer);
+	        }
+	    }
+
+	}
+
+
